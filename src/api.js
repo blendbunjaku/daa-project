06b52b76c9ab0e60ac743d4cfd90d2d7846d5799
GresaPlakolli <GresaPlakolli@users.noreply.github.com>
2024-01-14 00:18:56 +0100

    Add:Implementation of API to retrive geographical data

---
+++ src/api.js
@@ -0,0 +1,6 @@
+const highWayExclude = ["footway", "street_lamp", "steps", "pedestrian", "track", "path"];
+/**
+ * 
+ * @param {Array} boundingBox array with 2 objects that have a latitude and longitude property 
+ * @returns {Promise<Response>}
+ */

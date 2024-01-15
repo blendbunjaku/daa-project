f3d1589bc40892811111798ba09bb9137e19e1d9
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2024-01-15 22:52:18 +0100

    implement map component ...

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,79 @@
 import DeckGL from "@deck.gl/react";
 import { Map as MapGL } from "react-map-gl";
+import maplibregl from "maplibre-gl";
 import { PolygonLayer, ScatterplotLayer } from "@deck.gl/layers";
 import { TripsLayer } from "@deck.gl/geo-layers";
+import { useEffect, useRef, useState } from "react";
+import { getBoundingBoxFromPolygon, getMapGraph, getNearestNode } from "../services/MapService";
 import { INITIAL_COLORS, INITIAL_VIEW_STATE, MAP_STYLE } from "../config";
+import useSmoothStateChange from "../hooks/useSmoothStateChange";
 
 function Map() {
     const [startNode, setStartNode] = useState(null);
     const [endNode, setEndNode] = useState(null);
     const [selectionRadius, setSelectionRadius] = useState([]);
     const [tripsData, setTripsData] = useState([]);
+    const [started, setStarted] = useState();
     const [time, setTime] = useState(0);
+    const [animationEnded, setAnimationEnded] = useState(false);
+    const [fadeRadiusReverse, setFadeRadiusReverse] = useState(false);
+    const [placeEnd, setPlaceEnd] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [colors, setColors] = useState(INITIAL_COLORS);
     const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+    const ui = useRef();
+    const fadeRadius = useRef();
     const selectionRadiusOpacity = useSmoothStateChange(0, 0, 1, 400, fadeRadius.current, fadeRadiusReverse);
+
+    async function mapClick(e, info, radius = null) {
+        if(started && !animationEnded) return;
+
+        setFadeRadiusReverse(false);
+        fadeRadius.current = true;
+        clearPath();
+
+        // Place end node
+        if(info.rightButton || placeEnd) {
+            if(e.layer?.id !== "selection-radius") {
+                ui.current.showSnack("Please select a point inside the radius.", "info");
+                return;
+            }
+
+            if(loading) {
+                ui.current.showSnack("Please wait for all data to load.", "info");
+                return;
+            }
+
+            const loadingHandle = setTimeout(() => {
+                setLoading(true);
+            }, 300);
+            
+            const node = await getNearestNode(e.coordinate[1], e.coordinate[0]);
+            if(!node) {
+                ui.current.showSnack("No path was found in the vicinity, please try another location.");
+                clearTimeout(loadingHandle);
+                setLoading(false);
+                return;
+            }
+
+            const realEndNode = state.current.getNode(node.id);
+            setEndNode(node);
+            
+            clearTimeout(loadingHandle);
+            setLoading(false);
+
+            if(!realEndNode) {
+                ui.current.showSnack("An error occurred. Please try again.");
+                return;
+            }
+            state.current.endNode = realEndNode;
+            
+            return;
+        }
+
+
+
+    }
 
     return (
         <>
@@ -79,3 +140,5 @@ function Map() {
         </>
     );
 }
+
+export default Map;

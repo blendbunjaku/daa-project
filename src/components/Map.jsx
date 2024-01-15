c54cbe6013b0ddd0db453a2a3e293895d9e83b9a
ajsheselmani <ajshe.selmani@student.uni-pr.edu>
2024-01-15 21:37:22 +0100

    Implement map component

---
+++ src/components/Map.jsx
@@ -0,0 +1,81 @@
+import DeckGL from "@deck.gl/react";
+import { Map as MapGL } from "react-map-gl";
+import { PolygonLayer, ScatterplotLayer } from "@deck.gl/layers";
+import { TripsLayer } from "@deck.gl/geo-layers";
+import { INITIAL_COLORS, INITIAL_VIEW_STATE, MAP_STYLE } from "../config";
+
+function Map() {
+    const [startNode, setStartNode] = useState(null);
+    const [endNode, setEndNode] = useState(null);
+    const [selectionRadius, setSelectionRadius] = useState([]);
+    const [tripsData, setTripsData] = useState([]);
+    const [time, setTime] = useState(0);
+    const [colors, setColors] = useState(INITIAL_COLORS);
+    const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+    const selectionRadiusOpacity = useSmoothStateChange(0, 0, 1, 400, fadeRadius.current, fadeRadiusReverse);
+
+    return (
+        <>
+            <div onContextMenu={(e) => { e.preventDefault(); }}>
+                <DeckGL
+                    initialViewState={viewState}
+                    controller={{ doubleClickZoom: false, keyboard: false }}
+                    onClick={mapClick}
+                >
+                    <PolygonLayer 
+                        id={"selection-radius"}
+                        data={selectionRadius}
+                        pickable={true}
+                        stroked={true}
+                        getPolygon={d => d.contour}
+                        getFillColor={[80, 210, 0, 10]}
+                        getLineColor={[9, 142, 46, 175]}
+                        getLineWidth={3}
+                        opacity={selectionRadiusOpacity}
+                    />
+
+                    <TripsLayer
+                        id={"pathfinding-layer"}
+                        data={tripsData}
+                        opacity={1}
+                        widthMinPixels={3}
+                        widthMaxPixels={5}
+                        fadeTrail={false}
+                        currentTime={time}
+                        getColor={d => colors[d.color]}
+                        updateTriggers={{
+                            getColor: [colors.path, colors.route]
+                        }}
+                    />
+
+                    <ScatterplotLayer 
+                        id="start-end-points"
+                        data={[
+                            ...(startNode ? [{ coordinates: [startNode.lon, startNode.lat], color: colors.startNodeFill, lineColor: colors.startNodeBorder }] : []),
+                            ...(endNode ? [{ coordinates: [endNode.lon, endNode.lat], color: colors.endNodeFill, lineColor: colors.endNodeBorder }] : []),
+                        ]}
+                        pickable={true}get
+                        opacity={1}
+                        stroked={true}
+                        filled={true}
+                        radiusScale={1}
+                        radiusMinPixels={7}
+                        radiusMaxPixels={20}
+                        lineWidthMinPixels={1}
+                        lineWidthMaxPixels={3}
+                        getPosition={d => d.coordinates}
+                        getFillColor={d => d.color}
+                        getLineColor={d => d.lineColor}
+                    />
+
+                    <MapGL 
+                        reuseMaps mapLib={maplibregl} 
+                        mapStyle={MAP_STYLE} 
+                        doubleClickZoom={false}
+                    />
+                </DeckGL>
+
+            </div>
+        </>
+    );
+}

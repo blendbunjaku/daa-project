97e3404a29cb5504af23dd6b33185120677a0ac6
dionamuciqi <diona.muqiqi3@gmail.com>
2024-01-16 21:03:14 +0100

    implement drawer's menu

---
+++ src/components/Interface.jsx
@@ -0,0 +1,175 @@
+import { Button, IconButton, Typography, Snackbar, Alert, CircularProgress, Fade, Tooltip, Drawer, MenuItem, Select, InputLabel, FormControl, Menu, Backdrop, Stepper, Step, StepLabel } from "@mui/material";
+import { MuiColorInput } from "mui-color-input";
+import { PlayArrow, Settings, Movie, Pause, Replay } from "@mui/icons-material";
+import { INITIAL_COLORS, LOCATIONS } from "../config";
+import { arrayToRgb, rgbToArray } from "../helpers";
+
+
+<Drawer
+                className={`side-drawer ${cinematic ? "cinematic" : ""}`}
+                anchor="left"
+                open={sidebar}
+                onClose={() => {setSidebar(false);}}
+            >
+                <div className="sidebar-container">
+
+                    <FormControl variant="filled">
+                        <InputLabel style={{ fontSize: 14 }} id="algo-select">Algorithm</InputLabel>
+                        <Select
+                            labelId="algo-select"
+                            value={settings.algorithm}
+                            onChange={e => {changeAlgorithm(e.target.value);}}
+                            required
+                            style={{ backgroundColor: "#404156", color: "#fff", width: "100%", paddingLeft: 1 }}
+                            inputProps={{MenuProps: {MenuListProps: {sx: {backgroundColor: "#404156"}}}}}
+                            size="small"
+                            disabled={!animationEnded && started}
+                        >
+                            <MenuItem value={"astar"}>A* algorithm</MenuItem>
+                            <MenuItem value={"greedy"}>Greedy algorithm</MenuItem>
+                            <MenuItem value={"dijkstra"}>Dijkstra&apos;s algorithm</MenuItem>
+                            <MenuItem value={"bidirectional"}>Bidirectional Search algorithm</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    <div>
+                        <Button
+                            id="locations-button"
+                            aria-controls={menuOpen ? "locations-menu" : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={menuOpen ? "true" : undefined}
+                            onClick={(e) => {setMenuAnchor(e.currentTarget);}}
+                            variant="contained"
+                            disableElevation
+                            style={{ backgroundColor: "#404156", color: "#fff", textTransform: "none", fontSize: 16, paddingBlock: 8, justifyContent: "start" }}
+                        >
+                            Locations
+                        </Button>
+                        <Menu
+                            id="locations-menu"
+                            anchorEl={menuAnchor}
+                            open={menuOpen}
+                            onClose={() => {setMenuAnchor(null);}}
+                            MenuListProps={{
+                                "aria-labelledby": "locations-button",
+                                sx: {
+                                    backgroundColor: "#404156"
+                                }
+                            }}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                        >
+                            {LOCATIONS.map(location => 
+                                <MenuItem key={location.name} onClick={() => {
+                                    closeMenu();
+                                    changeLocation(location);
+                                }}>{location.name}</MenuItem>
+                            )}
+                        </Menu>
+                    </div>
+
+                    <div className="styles-container">
+                        <Typography style={{ color: "#A8AFB3", textTransform: "uppercase", fontSize: 14 }} >
+                            Styles
+                        </Typography>
+                        
+                        <div>
+                            <Typography id="start-fill-label" >
+                                Start node fill color
+                            </Typography>
+                            <div className="color-container">
+                                <MuiColorInput value={arrayToRgb(colors.startNodeFill)} onChange={v => {setColors({...colors, startNodeFill: rgbToArray(v)});}} aria-labelledby="start-fill-label" style={{ backgroundColor: "#404156" }} />
+                                <IconButton onClick={() => {setColors({...colors, startNodeFill: INITIAL_COLORS.startNodeFill});}} style={{ backgroundColor: "transparent" }} size="small">
+                                    <Replay style={{ color: "#fff", width: 20, height: 20 }} fontSize="inherit" />
+                                </IconButton>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Typography id="start-border-label" >
+                                Start node border color
+                            </Typography>
+                            <div className="color-container">
+                                <MuiColorInput value={arrayToRgb(colors.startNodeBorder)} onChange={v => {setColors({...colors, startNodeBorder: rgbToArray(v)});}} aria-labelledby="start-border-label" style={{ backgroundColor: "#404156" }} />
+                                <IconButton onClick={() => {setColors({...colors, startNodeBorder: INITIAL_COLORS.startNodeBorder});}} style={{ backgroundColor: "transparent" }} size="small">
+                                    <Replay style={{ color: "#fff", width: 20, height: 20 }} fontSize="inherit" />
+                                </IconButton>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Typography id="end-fill-label" >
+                                End node fill color
+                            </Typography>
+                            <div className="color-container">
+                                <MuiColorInput value={arrayToRgb(colors.endNodeFill)} onChange={v => {setColors({...colors, endNodeFill: rgbToArray(v)});}} aria-labelledby="end-fill-label" style={{ backgroundColor: "#404156" }} />
+                                <IconButton onClick={() => {setColors({...colors, endNodeFill: INITIAL_COLORS.endNodeFill});}} style={{ backgroundColor: "transparent" }} size="small">
+                                    <Replay style={{ color: "#fff", width: 20, height: 20 }} fontSize="inherit" />
+                                </IconButton>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Typography id="end-border-label" >
+                                End node border color
+                            </Typography>
+                            <div className="color-container">
+                                <MuiColorInput value={arrayToRgb(colors.endNodeBorder)} onChange={v => {setColors({...colors, endNodeBorder: rgbToArray(v)});}} aria-labelledby="end-border-label" style={{ backgroundColor: "#404156" }} />
+                                <IconButton onClick={() => {setColors({...colors, endNodeBorder: INITIAL_COLORS.endNodeBorder});}} style={{ backgroundColor: "transparent" }} size="small">
+                                    <Replay style={{ color: "#fff", width: 20, height: 20 }} fontSize="inherit" />
+                                </IconButton>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Typography id="path-label" >
+                                Path color
+                            </Typography>
+                            <div className="color-container">
+                                <MuiColorInput value={arrayToRgb(colors.path)} onChange={v => {setColors({...colors, path: rgbToArray(v)});}} aria-labelledby="path-label" style={{ backgroundColor: "#404156" }} />
+                                <IconButton onClick={() => {setColors({...colors, path: INITIAL_COLORS.path});}} style={{ backgroundColor: "transparent" }} size="small">
+                                    <Replay style={{ color: "#fff", width: 20, height: 20 }} fontSize="inherit" />
+                                </IconButton>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Typography id="route-label" >
+                                Shortest route color
+                            </Typography>
+                            <div className="color-container">
+                                <MuiColorInput value={arrayToRgb(colors.route)} onChange={v => {setColors({...colors, route: rgbToArray(v)});}} aria-labelledby="route-label" style={{ backgroundColor: "#404156" }} />
+                                <IconButton onClick={() => {setColors({...colors, route: INITIAL_COLORS.route});}} style={{ backgroundColor: "transparent" }} size="small">
+                                    <Replay style={{ color: "#fff", width: 20, height: 20 }} fontSize="inherit" />
+                                </IconButton>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="shortcuts-container">
+                        <Typography style={{ color: "#A8AFB3", textTransform: "uppercase", fontSize: 14 }} >
+                            Shortcuts
+                        </Typography>
+
+                        <div className="shortcut">
+                            <p>SPACE</p>
+                            <p>Start/Stop animation</p>
+                        </div>
+                        <div className="shortcut">
+                            <p>R</p>
+                            <p>Clear path</p>
+                        </div>
+                        <div className="shortcut">
+                            <p>Arrows</p>
+                            <p>Animation playback</p>
+                        </div>
+                        <Button onClick={() => {setActiveStep(0);setShowTutorial(true);}}
+                            variant="contained" style={{ backgroundColor: "#404156", color: "#fff" }}
+                        >
+                            Show tutorial
+                        </Button>
+                    </div>
+                </div>
+            </Drawer>
